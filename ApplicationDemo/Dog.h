37df9329ff44cba5e6a28dbfbde63cdb748beb0e
ASSASSINYM <ASSASSINYM@users.noreply.github.com>
2020-12-14 18:32:52 +0800

    Update Dog.h
    远程修改 dog 类 去掉 sex ,增加 age

--- ApplicationDemo/Dog.h
+++ ApplicationDemo/Dog.h
@@ -12,7 +12,7 @@ NS_ASSUME_NONNULL_BEGIN
 @interface Dog : NSObject
 
 @property(nonatomic,copy)NSString *name;
-@property(nonatomic,copy)NSString *sex;
+@property(nonatomic,copy)NSString *age
 
 @end
 
